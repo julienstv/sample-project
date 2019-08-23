0c2c6a80e03cd27a26d8e33027e2f90f1d3aa613
Fabian Bouché <fabian.bouche@gmail.com>
2019-08-23 23:23:32 +0200

    Added dependency

--- src/main/java/org/bouche/training/sample/maven/HelloWorld.java
+++ src/main/java/org/bouche/training/sample/maven/HelloWorld.java
@@ -1,9 +1,11 @@
 package org.bouche.training.sample.maven;
 
+import org.bouche.training.sample.common.MyClass;
+
 public class HelloWorld {
 	
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		System.out.println(MyClass.HELLO_WORLD);
 	}
 
 }
